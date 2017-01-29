41051ec4dd8af5d4048574602a5eedb68ae0bb1c
Brent <karmapoint@Brents-MacBook-Air.local>
2017-01-28 22:13:21 -0800

    pass simon specs

--- w2d1/skeleton/lib/simon.rb
+++ w2d1/skeleton/lib/simon.rb
@@ -27,7 +27,7 @@ class Simon
   end
 
   def show_sequence
-
+    add_random_color
   end
 
   def require_sequence
@@ -47,6 +47,8 @@ class Simon
   end
 
   def reset_game
-
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
 end

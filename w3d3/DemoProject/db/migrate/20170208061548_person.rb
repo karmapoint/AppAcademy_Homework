3cf59586d26943d083b211e13fbba9f23468da2f
Brent <karmapoint@Brents-MacBook-Air.local>
2017-02-12 15:43:24 -0800

    Remove all non-web files

--- w3d3/DemoProject/db/migrate/20170208061548_person.rb
+++
@@ -1,9 +0,0 @@
-class Person < ActiveRecord::Migration
-  def change
-    create_table :people do |t|
-      t.string :name, null: false
-      t.integer :house_id, null: false
-      t.timestamps
-    end
-  end
-end
--- w3d3/DemoProject/db/migrate/20170208062954_house.rb
+++
@@ -1,8 +0,0 @@
-class House < ActiveRecord::Migration
-  def change
-    create_table :houses do |t|
-      t.string :address, null: false
-      t.timestamps
-    end
-  end
-end

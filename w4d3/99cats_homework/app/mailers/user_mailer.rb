863f8964cf4b802e6ce6ae170809d070b9591759
Brent <karmapoint@gmail.com>
2017-02-15 21:07:29 -0800

    complete helpers, layouts, partials homework

---
+++ w4d3/99cats_homework/app/mailers/user_mailer.rb
@@ -0,0 +1,10 @@
+class UserMailer < ApplicationMailer
+  default from: 'everybody@appacademy.io'
+
+  def welcome_email(user)
+    @user = user
+    @url = 'http://www.99cats.com'
+    mail(to: user.username, subject: 'Welcome to 99cats!')
+  end
+
+end

68205d9a1c7478a03c23e5bb4082b200697ff8a7
Brent <karmapoint@gmail.com>
2017-03-05 16:08:52 -0800

    add store and actions creator

---
+++ w7d1/currency/frontend/actions.js
@@ -0,0 +1,8 @@
+export const SWITCH_CURRENCY = "SWITCH_CURRENCY";
+
+export const selectCurrency = (baseCurrency, rates) => ({
+  type: SWITCH_CURRENCY,
+
+});
+
+export default selectCurrency; 
--- w7d1/currency/webpack.config.js
+++ w7d1/currency/webpack.config.js
@@ -5,14 +5,14 @@ module.exports = {
   context: __dirname,
   entry: "./frontend/entry.jsx",
   output: {
-    path: path.resolve(__dirname),  
+    path: path.resolve(__dirname),
     filename: "bundle.js"
   },
   module: {
     loaders: [
       {
-        test: [/\.jsx?$/],
-        exclude: /node_modules/,
+        test: [/\.jsx?$/, [/\.js?$/]],
+        exclude: /(node_modules|bower_components)/,
         loader: 'babel-loader',
         query: {
           presets: ['react', 'es2015']

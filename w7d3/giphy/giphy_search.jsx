e4b4c76a14754b09357366316998776603e5a8a1
Brent <karmapoint@gmail.com>
2017-03-07 22:22:08 -0800

    complete redux cycle

--- w7d3/giphy/giphy_search.jsx
+++ w7d3/giphy/giphy_search.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
-import { fetchSearchGiphys } from './util/api_util';
-import { receiveSearchGiphys } from './actions/giphy_actions';
+// import { fetchSearchGiphys } from './util/api_util';
+import { receiveSearchGiphys, fetchSearchGiphys } from './actions/giphy_actions';
 
 const store = configureStore();
 

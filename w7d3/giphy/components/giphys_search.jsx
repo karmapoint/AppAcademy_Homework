38cccec90769570b8707dc13cf35e337023bfd54
Brent <karmapoint@gmail.com>
2017-03-07 23:12:09 -0800

    complete working version

--- w7d3/giphy/components/giphys_search.jsx
+++ w7d3/giphy/components/giphys_search.jsx
@@ -1,2 +1,42 @@
 import React from 'react';
 import GiphysIndex from './giphys_index';
+
+class GiphysSearch extends React.Component {
+
+  constructor() {
+    super();
+    this.state = { searchTerm: 'kitten' };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount(){
+    this.props.fetchSearchGiphys('kitten');
+  }
+
+  handleChange(event) {
+    this.setState({ searchTerm: event.currentTarget.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    let searchTerm = this.state.searchTerm.split(' ').join('+');
+    this.props.fetchSearchGiphys(searchTerm);
+  }
+
+  render() {
+    let { giphys }= this.props;
+
+    return (
+    <div>
+      <form className="search-bar">
+        <input value={this.state.searchTerm} onChange={this.handleChange} />
+        <button type="submit" onClick={this.handleSubmit}>Search Giphy</button>
+      </form>
+      <GiphysIndex giphys={giphys} />
+    </div>
+  );
+  }
+}
+
+export default GiphysSearch;
--- w7d3/giphy/giphy_search.jsx
+++ w7d3/giphy/giphy_search.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
-// import { fetchSearchGiphys } from './util/api_util';
 import { receiveSearchGiphys, fetchSearchGiphys } from './actions/giphy_actions';
 
-const store = configureStore();
 
-window.store = store;
-window.fetchSearchGiphys = fetchSearchGiphys;
-window.receiveSearchGiphys = receiveSearchGiphys;
+document.addEventListener('DOMContentLoaded', () => {
+    const store = configureStore();
+    const root = document.getElementById('root');
+    ReactDOM.render(<Root store={store} />, root);
+});
+  
